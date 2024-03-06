7440553698b40c4cf3d9e1675758ff00b8a03173
Kacper Mikolajczyk <kacper.mikolajczyk1903@gmail.com>
2024-03-06 15:10:07 +0100

    Przeniesienie plikow

--- z3.py
+++
@@ -1,8 +0,0 @@
-globalna = "A to zmienna globalna"
-
-def zadanie3():
-    print("_____ZADANIE 3_____")    
-    funkcyjna = "To jest zmienna funkcyjna"
-
-    print(funkcyjna)
-    print(globalna)
--- z4.py
+++
@@ -1,8 +0,0 @@
-
-
-def zadanie4(func, array):
-    print("_____ZADANIE 4_____")
-    print("Tablica: "+str(list(array)))
-    arraysqr = map(func, array)
-
-    print("Wynik: "+str(list(arraysqr)))
